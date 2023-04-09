87220893a24d469611dd8d4d241b64d74c9d2256
TheUnknown-Programmer <106790970+TheUnknown-Programmer@users.noreply.github.com>
2023-04-09 07:45:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ client/src/components/Categories/Categories.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import "./Categories.scss";
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <div className="col">
+        <div className="row">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1673458070037-8cc98a465c0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2555&q=80"
+            alt=""
+          />
+          <button>
+            <Link className="link" to="/products/1">
+              Sale
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col">
+        <div className="row">
+          {" "}
+          <img
+            src="https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              New Season
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col col-l">
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <img
+                src="https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <button>
+                <Link to="/products/1" className="link">
+                  Men
+                </Link>
+              </button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              {" "}
+              <img
+                src="https://images.pexels.com/photos/2703202/pexels-photo-2703202.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <button>
+                <Link to="/products/1" className="link">
+                  Accessories
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/1159670/pexels-photo-1159670.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              Shoes
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
